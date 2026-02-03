325e34de3def4091941a175fd244acb4d50291b2
hitomikkusu-design <hitomikkusu@gmail.com>
2026-02-03 15:23:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+from fastapi import Request
+
+@app.post("/callback")
+async def callback(request: Request):
+    body = await request.body()
+    print(body)
+    return {"status": "ok"}
 
 @app.get("/")
 def read_root():
